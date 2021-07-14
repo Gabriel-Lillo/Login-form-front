552a8a297177181832b4c245326071b6a6ef2279
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2021-07-14 00:19:35 -0400

    login

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './app.css'
+import Login from "./pages/Login";
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Login />
+        
+        
+        
+        
+        </BrowserRouter>
+    )
+}
+
+export default App;
